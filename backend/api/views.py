8e3d91d99fb3812c60f036ff8981c32b2cf6799f
Sebastian Ozimkowski <46507296+YoungSebastian@users.noreply.github.com>
2021-05-23 14:15:57 +0200

    Feature/get properties with details (#6)
    * Add sort by price,baths,beds min/max
    Add sort by property_type, sale_type, days_old
    Add pytest to views
    Add sale_type field to Property
    
    * format, flake8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.contrib.gis.geos import Polygon
+from django.utils import timezone
 from rest_framework import mixins, status, viewsets
 from rest_framework.response import Response
 
@@ -30,9 +31,65 @@ class PropertyViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
             bbox = geojson.get("coordinates")
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
         poly = Polygon(tuple((x, y) for x, y in bbox[0]))
         query = Property.objects.filter(cordinates__intersects=poly)
+
+        sale_type = request.query_params.get("sale_type")
+        if sale_type:
+            query = query.filter(sale_type=sale_type)
+
+        property_type = request.query_params.get("property_type")
+        if property_type:
+            query = query.filter(property_type=property_type)
+
+        price_min = request.query_params.get("price_min")
+        price_max = request.query_params.get("price_max")
+        if price_max and price_min:
+            query = query.filter(price__lte=int(price_max), price__gte=int(price_min))
+        elif price_max:
+            query = query.filter(price__lte=int(price_max))
+        elif price_min:
+            query = query.filter(price__gte=int(price_min))
+
+        bedrooms_min = request.query_params.get("bedrooms_min")
+        bedrooms_max = request.query_params.get("bedrooms_max")
+        if bedrooms_max and bedrooms_min:
+            query = query.filter(
+                bedrooms__lte=int(bedrooms_max), bedrooms__gte=int(bedrooms_min)
+            )
+        elif bedrooms_max:
+            query = query.filter(bedrooms__lte=int(bedrooms_max))
+        elif bedrooms_min:
+            query = query.filter(bedrooms__gte=int(bedrooms_min))
+
+        bathrooms_min = request.query_params.get("bathrooms_min")
+        bathrooms_max = request.query_params.get("bathrooms_max")
+        if bathrooms_max and bathrooms_min:
+            query = query.filter(
+                bathrooms__lte=int(bathrooms_max), bathrooms__gte=int(bathrooms_min)
+            )
+        elif bathrooms_max:
+            query = query.filter(bathrooms__lte=int(bathrooms_max))
+        elif bathrooms_min:
+            query = query.filter(bathrooms__gte=int(bathrooms_min))
+
+        days_old = request.query_params.get("days_old")
+        if days_old == "1d":
+            date = timezone.now() + timezone.timedelta(1)
+            query = query.filter(date__lt=date)
+        elif days_old == "3d":
+            date = timezone.now() + timezone.timedelta(3)
+            query = query.filter(date__lt=date)
+        elif days_old == "7d":
+            date = timezone.now() + timezone.timedelta(7)
+            query = query.filter(date__lt=date)
+        elif days_old == "14d":
+            date = timezone.now() + timezone.timedelta(14)
+            query = query.filter(date__lt=date)
+        elif days_old == "30d":
+            date = timezone.now() + timezone.timedelta(30)
+            query = query.filter(date__lt=date)
+
         serializer = PropertySerializer(query, many=True)
         ctx = {"properties": serializer.data, "cordinates": bbox}
         return Response(ctx, status=status.HTTP_200_OK)
