1062a6f3a7f03d58accb39468a31e78bdf2c2b7f
Sebastian Ozimkowski <46507296+YoungSebastian@users.noreply.github.com>
2021-05-26 12:59:50 +0200

    Feature/add property view docs (#7)
    * fix flake8
    
    * Add docs
    
    * fix for: test for membership should be 'not in'

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +11,19 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("api.urls")),
 ]
+
+if settings.DEBUG:
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Willos REST API",
+            default_version="v1",
+        )
+    )
+
+    urlpatterns += [
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+    ]
